ca63a80922b22e8e4cf726dda38e4e9ecfd3320f
sank2000 <santhoshvelr@yahoo.com>
2021-01-05 12:06:13 +0530

    account screen and lisiting screen added

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 // import HomeScreen from './app/screen/HomeScreen';
 // import ViewImage from './app/screen/ViewImage';
 // import ListingDetailsScreen from './app/screen/ListingDetailsScreen';
-// import ListingDetailsScreen from './app/screen/ListingDetailsScreen';
-import MessagesScreen from './app/screen/MessagesScreen';
+import ListingsScreen from './app/screen/ListingsScreen';
+// import MessagesScreen from './app/screen/MessagesScreen';
+// import AccountScreen from './app/screen/AccountScreen';
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingsScreen />;
 }
